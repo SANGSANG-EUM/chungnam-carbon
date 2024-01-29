7865cd64018e69ea901382f92cf4492619167633
onesick <onesick@iium.kr>
2024-01-29 10:44:18 +0900

    sub 경제관련 반응형 완료
    -미래경제
    -전환경제
    -순환경제
    -생활경제
    -탄소중립국가허브

--- src/js/normal.js
+++ src/js/normal.js
@@ -23,10 +23,11 @@ $(document).ready(function () {
   };
   const mainVisualSlider = f.slider(mainVisualTarget, mainVisualOptions);
 
+  //관련 이미지 슬라이드
   const relImgSlideTarget = '.rel_img_sl .swiper-container';
   const relImgSlideOptions = {
-    spaceBetween: 40,
-    slidesPerView: 4,
+    spaceBetween: 0,
+    slidesPerView: 1,
     watchSlidesProgress: true,
     loop: false,
     centeredSlides: false,
@@ -38,6 +39,22 @@ $(document).ready(function () {
       nextEl: ".rel_img_slide_btn_box .next",
       prevEl: ".rel_img_slide_btn_box .prev",
     },
+    breakpoints: {
+      481: {
+        spaceBetween: 20,
+        slidesPerView: 2,
+      },
+      768: {
+        spaceBetween: 20,
+        slidesPerView: 3,
+      },
+      1291: {
+        slidesPerView: 4,
+      },
+      1441: {
+        spaceBetween: 40,
+      }
+    }
   };
   const relImgSlider = f.slider(relImgSlideTarget, relImgSlideOptions);
 });
